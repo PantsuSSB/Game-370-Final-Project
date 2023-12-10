49c7b7249a6857938be34f0f873c7239e33b42f0
yeij <inehrbass@live.com>
2023-12-10 05:06:52 -0500

    added enemies, skybox, music, and menus

--- Western First Person Shooter/Assets/Scripts/Scene Manager/SceneScript.cs
+++ Western First Person Shooter/Assets/Scripts/Scene Manager/SceneScript.cs
@@ -6,17 +6,7 @@ using UnityEngine.UI;
 
 public class SceneScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //following three functions load specific scenes or quits the game for menu buttons
     public void StartGame()
     {
         SceneManager.LoadScene("TestScene");
@@ -24,5 +14,17 @@ public class SceneScript : MonoBehaviour
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    //this is for the end of the level when the player enters a trigger to win the game
+    private void OnTriggerEnter(Collider winScreen)
+    {
+        if (winScreen.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("WinScreen");
+        }
     }
 }
